af9f5da6d7c1c45b67cbfe65b5a5f13c66394fa5
hsieha <anthony.hsieh1@gmail.com>
2015-04-20 14:55:32 -0400

    opening up in unity creates misc files :(

---
+++ Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		PhotonNetwork.ConnectUsingSettings("0.1");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
