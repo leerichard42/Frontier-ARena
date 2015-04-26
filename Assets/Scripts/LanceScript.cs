f7fa9dce92e30dd551226f8b2dc582ebf7f6bc0e
Richard <lerich+bitbucket@seas.upenn.edu>
2015-04-26 19:43:09 -0400

    knight, instacharge, hitboxes

---
+++ Assets/Scripts/LanceScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LanceScript : MonoBehaviour {
+	GameObject player;
+	
+	// Use this for initialization
+	void Start () {
+		player = transform.parent.gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
