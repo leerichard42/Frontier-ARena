d33eb944658aa098eb0a992566038dd3add3996a
Richard <lerich+bitbucket@seas.upenn.edu>
2015-04-25 23:42:13 -0400

    Power ups, lives

---
+++ Assets/Scripts/ChargeBarScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChargeBarScript : MonoBehaviour {
+
+	public Texture2D ChargeBarTexture;
+	private PlayerManager player;
+	private Vector3 screenPoint;
+
+	void Start(){
+		player = gameObject.GetComponent < PlayerManager> ();
+	}
+
+	void OnGUI () {
+		if (player) {
+			if (player.chargeTimer > 0) {
+				GUI.DrawTexture (new Rect (0, Screen.height - 50, Screen.width * player.chargeTimer / 2, 50), ChargeBarTexture, ScaleMode.StretchToFill, true, 0);
+			}
+		}
+		if (player.livesLeft > 0) {
+				GUI.DrawTexture (new Rect (10, 10, 25, 25), ChargeBarTexture, ScaleMode.StretchToFill, true, 0);
+		}
+		if (player.livesLeft > 1) {
+			GUI.DrawTexture (new Rect (45, 10, 25, 25), ChargeBarTexture, ScaleMode.StretchToFill, true, 0);
+		}
+		if (player.livesLeft > 2) {
+			GUI.DrawTexture (new Rect (80, 10, 25, 25), ChargeBarTexture, ScaleMode.StretchToFill, true, 0);
+		}
+	}
+}
---
+++ Assets/Scripts/HitboxScript.cs~
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HitboxScript : MonoBehaviour {
+	GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		player = transform.parent.gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider collider){
+		GameObject obj = collider.gameObject;
+		if (obj != player && obj.tag == "Player") {
+//			Debug.Log("HIT");
+			player.GetComponent<PlayerManager>().hit();
+		}
+	}
+}
