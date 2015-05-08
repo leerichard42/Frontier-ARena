8617600fb9311ca3d68f397ffa9f726bc09ea818
JivingTechnostic <jiatong@seas.upenn.edu>
2015-05-08 07:03:47 -0400

    Added audience stuff
    added audience prefab and script

---
+++ Assets/Scripts/AudienceScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudienceScript : MonoBehaviour {
+	public Animator [] controllers;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void Cheer() {
+		// play cheer sound
+		foreach (Animator a in controllers) {
+			a.SetTrigger ("Cheer");
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
